18a5d1e6541dc59640b9ebc63895d39176cef505
karam chauhan <karam.chauhan86@gmail.com>
2022-03-26 17:50:42 +0100

    adjusted for api changes

--- src/clients/launchpad/videoPlayerClient.ts
+++ src/clients/launchpad/videoPlayerClient.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
-import {VideoFileResponse, VideoPlayerExtensionsResponse, VideoPlayerStateResponse} from "./types";
+import {VideoFileResponse, VideoPlayerExtensionsResponse} from "./types";
 
 export function openVideo(id: Number): Promise<VideoFileResponse> {
     return axios.post('/api/v1/video/player/open/' + id)
         .then(response => response.data)
 }
 
-export function closeVideoPlayer(): Promise<VideoPlayerStateResponse> {
+export function closeVideoPlayer(): Promise<void> {
     return axios.post('/api/v1/video/player/close')
         .then(response => response.data);
 }
@@ -26,32 +26,32 @@ export function fetchVideoPlayerExtensions(): Promise<VideoPlayerExtensionsRespo
         .then(response => response.data)
 }
 
-export function toggleSubtitle(): Promise<VideoPlayerStateResponse> {
+export function toggleSubtitle(): Promise<void> {
     return axios.post('/api/v1/video/player/extension/toggle-subtitle')
         .then(response => response.data)
 }
 
-export function skipForward(): Promise<VideoPlayerStateResponse> {
+export function skipForward(): Promise<void> {
     return axios.post('/api/v1/video/player/extension/skip-forward')
         .then(response => response.data)
 }
 
-export function skipBackward(): Promise<VideoPlayerStateResponse> {
+export function skipBackward(): Promise<void> {
     return axios.post('/api/v1/video/player/extension/skip-backward')
         .then(response => response.data)
 }
 
-export function pauseResume(): Promise<VideoPlayerStateResponse> {
+export function pauseResume(): Promise<void> {
     return axios.post('/api/v1/video/player/extension/pause-resume')
         .then(response => response.data)
 }
 
-export function cycleSubtitles(): Promise<VideoPlayerStateResponse> {
+export function cycleSubtitles(): Promise<void> {
     return axios.post('/api/v1/video/player/extension/cycle-subtitles')
         .then(response => response.data)
 }
 
-export function cycleAudioTrack(): Promise<VideoPlayerStateResponse> {
+export function cycleAudioTrack(): Promise<void> {
     return axios.post('/api/v1/video/player/extension/cycle-audio-track')
         .then(response => response.data)
 }
